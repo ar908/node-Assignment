d820297d682cb64ce8c14ba492dcad5fdb3b12a0
ar908 <96685835+ar908@users.noreply.github.com>
2022-02-21 12:41:47 +0530

    Assignment -1 sub

--- assignments/assignment_1/index.js
+++ assignments/assignment_1/index.js
@@ -1,13 +1,20 @@
 function getNameFromCommandLine() {
     // Write you code here, name should be taken as args in process.argv
-}
+    return process.argv[5]
+}   
 
 function getNameFromEnv() {
     // Write your code here
+    return process.env.name
 }
 
 function getNameFromReadLine() {
     // Write your code here
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+
 }
 
 module.exports = {
